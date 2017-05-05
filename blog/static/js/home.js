f262bd5c558de8d4498cca49a695ceee681f5ae2
Your Name <you@example.com>
2017-05-05 15:40:03 +0800

    My Django Girls app, first commit

--- blog/static/js/home.js
+++ blog/static/js/home.js
@@ -1,12 +1,9 @@
 function home(){
     location.href = "/";
 }
-function shopping(){
-    location.href = "#";
+function shoppingList(){
+    location.href = '/shopping_list';
 }
 function cart() {
     location.href = "#"
-}
-function homePage() {
-    location.href = "/"
 }
--- blog/views.py
+++ blog/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def post_list(request):
+
     return render(request, 'blog/post_list.html', {})
+def shopping_list(request):
+    return render(request, 'blog/shopping_list.html')
