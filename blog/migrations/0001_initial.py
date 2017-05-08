29e2ddd162c8a47a5075515158836a682fa9a118
Your Name <you@example.com>
2017-05-08 17:28:40 +0800

    My Django Girls app, first commit

--- blog/migrations/0001_initial.py
+++ blog/migrations/0001_initial.py
@@ -2,26 +2,22 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-from django.conf import settings
-import django.utils.timezone
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Post',
+            name='Goods',
             fields=[
-                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
-                ('title', models.CharField(max_length=200)),
-                ('text', models.TextField()),
-                ('created_date', models.DateTimeField(default=django.utils.timezone.now)),
-                ('published_date', models.DateTimeField(null=True, blank=True)),
-                ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+                ('id', models.AutoField(serialize=False, auto_created=True, primary_key=True, verbose_name='ID')),
+                ('type', models.CharField(max_length=200)),
+                ('name', models.CharField(max_length=200)),
+                ('price', models.FloatField(default='')),
+                ('unit', models.CharField(max_length=200)),
             ],
         ),
     ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import Goods
 
 # Create your views here.
 def post_list(request):
 
     return render(request, 'blog/post_list.html', {})
 def shopping_list(request):
-    return render(request, 'blog/shopping_list.html')
+    goods = Goods.objects.all()
+    return render(request, 'blog/shopping_list.html', {'goods': goods})
