3e6ffebe539c5beb8e3b55d3df9b009a0b1920ae
Your Name <you@example.com>
2017-05-15 18:06:18 +0800

    Changed the HTML for the site.

--- blog/static/js/shop.js
+++
@@ -1,6 +0,0 @@
- var $p = $("navigationbar").click();
-        $p.toggle(function(){
-            $p.css("background-color:black","color:white");
-        },function(){
-            $p.css("background-color: #373737","color: #bebdc1");
-        })
